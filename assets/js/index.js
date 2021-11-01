e2504996c624a40cce57f585038d9aa174405095
Reland Boyle <reland.boyle@gmail.com>
2021-10-31 22:12:22 -0400

    Rendering messages from database

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,61 @@
+// select the unordered list of all messages
+const messageList = document.querySelector('#message-list');
+
+
+// immediately fetch all messages currently in the database
+(async function fetchAllMessages() {
+  const response = await fetch('/messages');
+  const allMessages = await response.json();
+  console.log(allMessages);
+  
+  allMessages.forEach((mess) => {
+    appendMessage(mess);
+  });
+})();
+
+
+// helper to create and append new list item for each database entry
+const appendMessage = (msg) => {
+  const messageList = document.querySelector('#message-list');
+
+  const newNoteElement = document.createElement('li');
+
+  const messTextDiv = document.createElement('div');
+  messTextDiv.classList.add('postedMessages');
+  messTextDiv.innerText = msg.message;
+  newNoteElement.append(messTextDiv);
+
+  const messDeleteButton = document.createElement('button');
+  messDeleteButton.innerText = 'Delete';
+  newNoteElement.append(messDeleteButton);
+
+  const messIdDiv = document.createElement('div');
+  messIdDiv.innerText = msg._id;
+  messIdDiv.style.display = 'none';
+  newNoteElement.append(messIdDiv);
+
+  messageList.append(newNoteElement);
+};
+
 
 // poll for new messages every two seconds
-setInterval(() => loadMessageList(), 2000);
-
-
-function loadMessageList() {
-  console.log('TEST');
-}
+// setInterval(() => fetchNewMessages(), 2000);
 
 
 
-function displayAllMessages () {
-  // select the unordered list element
+
+// // when the "Save" button is clicked
+// function saveNewNote() {
+//   // select the password and description elements
+//   const entryPass = document.querySelector('#pass');
+//   const entryDesc = document.querySelector('#desc');
+//   // store the user-entered password and description as an object
+//   const newNote = ( { password: entryPass.value, description: entryDesc.value } );
+//   console.log(newNote);
+// }
+
+// // when the "Delete" button is clicked
+// function deleteNote() {
+//   // select the note id
   
-  // iterate over all messages in the database
-  for (const message of allMessages) {
-    // for each message, append list item element to the unordered list element
-    const newNoteElement = document.createElement('li');
-    newNoteElement.innerHTML = '<button class="del" id="{message._id}" onclick="deleteNote()">Delete</button>';
-    newNoteElement.innerText = message.message;
-    messageList.appendChild(newNoteElement);
-  }
-}
-
-// when the "Save" button is clicked
-function saveNewNote() {
-  // select the password and description elements
-  const entryPass = document.querySelector('#pass');
-  const entryDesc = document.querySelector('#desc');
-  // store the user-entered password and description as an object
-  const newNote = ( { password: entryPass.value, description: entryDesc.value } );
-  console.log(newNote);
-}
-
-// when the "Delete" button is clicked
-function deleteNote() {
-  // select the note id
-  
-}
+// }
